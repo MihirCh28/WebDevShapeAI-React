be1e66aa16cfa0a865f19aa5f5ab90904a003303
Mihir Chakkarwar <mchakkarwar28@gmail.com>
2021-06-05 12:12:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReactJS</h1>
+      <br />
+      <p>
+        An amazing bootcamp organised again by none other than shapeAI on the
+        topic Bootstrap and ReactJS
+      </p>
+    </div>
+  );
+}
+
+export default Note;
